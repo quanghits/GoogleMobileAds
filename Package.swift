1441520c12e19a1623a124e6b4264fe1b4790e92
TA MINH QUANG <quang@TAs-Mac-mini.local>
2021-05-20 22:55:37 +0700

    Update links of reference

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
         .package(name: "GoogleAppMeasurement",
                  url: "https://github.com/google/GoogleAppMeasurement.git",
-                 from:  "7.0.0"
+                 from:  "8.0.0"
         ),
         .package(name: "GoogleUserMessagingPlatform",
                  url: "https://github.com/quanghits/GoogleUserMessagingPlatform.git",
