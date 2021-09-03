19f932245e7a3b88618c21430d50b3d69dcb2c7a
TA MINH QUANG <quang@TAs-Mac-mini.local>
2021-09-03 22:38:49 +0700

    Update /GoogleMobileAds.xcframework 8.10.0

--- Binary/GoogleMobileAds.xcframework/ios-arm64_x86_64-simulator/GoogleMobileAds.framework/Headers/GADAdSize.h
+++ Binary/GoogleMobileAds.xcframework/ios-arm64_x86_64-simulator/GoogleMobileAds.framework/Headers/GADAdSize.h
@@ -57,7 +57,40 @@ GAD_EXTERN GADAdSize const kGADAdSizeFluid;
 /// Invalid ad size marker.
 GAD_EXTERN GADAdSize const kGADAdSizeInvalid;
 
-#pragma mark Adaptive Sizes
+#pragma mark Inline Adaptive Sizes
+
+/// Returns a GADAdSize with the given width and the device's portrait height. This ad size
+/// allows Google servers to choose an optimal ad size less than or equal to the returned size. The
+/// exact size of the ad returned is passed through the banner's ad size delegate and is indicated
+/// by the banner's intrinsicContentSize. This ad size is most suitable for ads intended for scroll
+/// views.
+GAD_EXTERN GADAdSize GADPortraitInlineAdaptiveBannerAdSizeWithWidth(CGFloat width);
+
+/// Returns a GADAdSize with the given width and the device's landscape height. This ad size
+/// allows Google servers to choose an optimal ad size less than or equal to the returned size. The
+/// exact size of the ad returned is passed through the banner's ad size delegate and is indicated
+/// by the banner's intrinsicContentSize. This ad size is most suitable for ads intended for scroll
+/// views.
+GAD_EXTERN GADAdSize GADLandscapeInlineAdaptiveBannerAdSizeWithWidth(CGFloat width);
+
+/// Returns a GADAdSize with the given width and the device's height. This is a convenience
+/// function to return GADPortraitInlineAdaptiveBannerAdSizeWithWidth or
+/// GADLandscapeInlineAdaptiveBannerAdSizeWithWidth based on the current interface orientation.
+/// This function must be called on the main queue.
+GAD_EXTERN GADAdSize GADCurrentOrientationInlineAdaptiveBannerAdSizeWithWidth(CGFloat width);
+
+/// Returns a GADAdSize with the given width and max height. This ad size allows Google servers to
+/// choose an optimal ad size less than or equal to the returned size. The exact size of the ad
+/// returned is passed through the banner's ad size delegate and is indicated by the banner's
+/// intrinsicContentSize. This ad size is most suitable for ads intended for scroll views.
+///
+/// @param width The ad width.
+/// @param maxHeight The maximum height a loaded ad will have. Must be at least 32 px, but a max
+/// height of 50 px or higher is recommended.
+GAD_EXTERN GADAdSize GADInlineAdaptiveBannerAdSizeWithWidthAndMaxHeight(CGFloat width,
+                                                                        CGFloat maxHeight);
+
+#pragma mark Anchored Adaptive Sizes
 
 /// Returns a GADAdSize with the given width and a Google-optimized height to create a banner ad.
 /// The size returned has an aspect ratio similar to that of kGADAdSizeBanner, suitable for
